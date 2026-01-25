126453385457218e24e93ddda224310d52c24c1d
Shahul Hameed <sudhuman@simplesolve.com>
2026-01-26 01:01:13 +0530

    chore: cleanup

--- _site/run_local.ps1
+++
@@ -1,9 +0,0 @@
-# Run Jekyll locally using Docker
-echo "Starting Jekyll server at http://localhost:4000..."
-echo "Press Ctrl+C to stop."
-
-docker run --rm `
-  --volume "${PWD}:/srv/jekyll" `
-  -p 4000:4000 `
-  jekyll/jekyll:4.2.2 `
-  jekyll serve --watch --force_polling
